af82d4be95722c08880235836771c85a337f0458
ztpong83 <flashpsyco@hotmail.com>
2022-04-01 12:22:05 +0800

    Add project files.

---
+++ GitDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
